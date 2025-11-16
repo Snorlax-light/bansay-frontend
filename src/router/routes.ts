2d196f1e65d4142e3a03b0b2a37e29f4592a7568
lawi107 <lawi.nh107@s.msumain.edu.ph>
2025-11-15 21:06:04 -0800

    separate auth pages, delete old LoginRegister.vue,  and fix route paths

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,39 @@
-const routes = [
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/LoginRegister.vue') },
-      { path: 'student-dashboard', component: () => import('pages/Students/StudentDashboard.vue') },
-      { path: 'officer-dashboard', component: () => import('pages/Officer/OfficerDashboard.vue') },
-      { path: 'admin-dashboard', component: () => import('pages/Admin/AdminDashboard.vue') },
+      { path: '', redirect: '/login' },
+
+      {
+        path: 'login',
+        component: () => import('pages/login/LoginPage.vue'),
+      },
+      {
+        path: 'register',
+        component: () => import('pages/register/RegisterPage.vue'),
+      },
+
+      {
+        path: 'student-dashboard',
+        component: () => import('pages/student/StudentDashboard.vue'),
+      },
+      {
+        path: 'officer-dashboard',
+        component: () => import('pages/officer/OfficerDashboard.vue'),
+      },
+      {
+        path: 'admin-dashboard',
+        component: () => import('pages/admin/AdminDashboard.vue'),
+      },
     ],
+  },
+
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
   },
 ];
 
